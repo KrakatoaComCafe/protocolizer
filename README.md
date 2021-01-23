c2328c6d5679da749ea882fa77ad7b283d77aab9
KrakatoaComCafe <tiagovidotto@gmail.com>
2021-01-23 16:53:31 -0300

    feature/PROT-4 Added swagger2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Protocolizer
+
+Link: [swagger-ui](localhost:9666/swagger-ui.html) of this project
--- src/main/java/br/com/krakatoa/protocolizer/ProtocolizerApplication.java
+++ src/main/java/br/com/krakatoa/protocolizer/ProtocolizerApplication.java
@@ -2,12 +2,14 @@ package br.com.krakatoa.protocolizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ProtocolizerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProtocolizerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProtocolizerApplication.class, args);
+    }
 
 }
